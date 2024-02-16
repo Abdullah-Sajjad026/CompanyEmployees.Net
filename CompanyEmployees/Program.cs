1b357afed3bce035d91efff49a6a681dcafbefb2
Abdullah Sajjad <contact@abdullahsajjad.me>
2024-02-16 11:18:14 +0500

    feat: starting out onion architecture section

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager
+    .LoadConfiguration(
+        string.Concat(Directory.GetCurrentDirectory(), "/NLog.config")
+    );
+
+
+builder.Services.AddCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else app.UseHsts();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
